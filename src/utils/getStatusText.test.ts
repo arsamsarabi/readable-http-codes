b0c3215f71459ab6b0a95684ade7e48624ed37b8
Arsam Sarabi <arsamsarabi@me.com>
2022-05-10 16:28:03 +0100

    fixes tests

--- src/utils/getStatusText.test.ts
+++ src/utils/getStatusText.test.ts
@@ -1,18 +1,14 @@
 import each from 'jest-each'
 
-import * as AllStatusCodes from '../responses'
+import { allStatusCodes } from '../responses'
 import { getStatusText } from './getStatusText'
 
 describe('getStatusText()', () => {
-  const codes = Object.keys(AllStatusCodes).map((key: string) => {
-    return (AllStatusCodes as unknown as Record<string, number>)[key]
-  })
-
-  each(codes).it('should return a text for %s', (statusCode) => {
+  each(Object.values(allStatusCodes)).it('should return a text for %s', (statusCode) => {
     expect(getStatusText(statusCode)).toBeDefined()
   })
 
-  each(codes).it('should return a text for %s that includes a prefix', (statusCode) => {
+  each(Object.values(allStatusCodes)).it('should return a text for %s that includes a prefix', (statusCode) => {
     expect(getStatusText(statusCode, true)).toBeDefined()
     expect(getStatusText(statusCode, true)).toMatch(/^(INFO:|OK:|REDIRECT:|CLIENT ERROR:|SERVER ERROR:)/i)
   })
